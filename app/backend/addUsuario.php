cee92eff2b19191cc43e19c0e54634aaa64ea44b
César Oliveira <cesar.oliveira@uaiti.com.br>
2014-01-07 18:48:26 -0200

    Cadastro de usuário

---
+++ app/backend/addUsuario.php
@@ -0,0 +1,20 @@
+<?php
+	require_once('bootstrap.php');
+	require_once('database.php');
+	$db = conect();
+	if (is_object($db)) {
+		$usuario = json_decode(file_get_contents("php://input"), true);
+		$userOk = insert($db, 'usuario', $usuario);
+		if ($userOk) {
+			$status = array(
+				'status' => true,
+				'idusuario' => $userOk
+			);
+		} else {
+			$status = array('status' => false);
+		}
+	} else {
+		die("<h1> Não foi possível conectar-se com o banco de dados</h1><h2>#{$db['code']}: {$db['message']}</h2>");
+	}
+	echo json_encode($status);
+?>
