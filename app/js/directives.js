2adc4b56406fdc71db964863f40cb9b6605326d6
César Oliveira <cesar.oliveira@uaiti.com.br>
2014-01-08 16:41:36 -0200

    Diretiva de validação de camp único

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,22 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('myApp.directives', [])
+    .directive('ngUnique', [
+        '$http',
+        function($http) {
+            return {
+                restrict: 'A',
+                require: 'ngModel',
+                link: function(scope, elm, attrs, ctrl) {
+                    ctrl.$parsers.unshift(function(value) {
+                        var url = 'backend/validaUnique.php?v=' + value + '&f=' + attrs.ngUnique;
+                        $http.get(url)
+                            .success(function(data) {
+                                ctrl.$setValidity('unique', data.status);
+                            });
+                    });
+                }
+            }
+        }
+    ]);
