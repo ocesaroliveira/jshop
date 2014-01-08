7d6a85d4cacc4fc3ae3541f3f4b07929bfefab2e
César Oliveira <cesar.oliveira@uaiti.com.br>
2014-01-08 16:41:26 -0200

    Diretiva de validação de camp único

---
+++ app/backend/validaUnique.php
@@ -0,0 +1,22 @@
+<?php
+	require_once('bootstrap.php');
+	require_once('database.php');
+	define('TABLE', 0);
+	define('FIELD', 1);
+	$db = conect();
+	$dados = array('status' => false);
+	if (is_object($db)) {
+		if (!empty($_GET['v']) && !empty($_GET['f'])) {
+			$value = $_GET['v'];
+			$field = explode('.', $_GET['f']);
+			if (count($field) == 2) {
+				$table = $field[TABLE];
+				$field = $field[FIELD];
+				$res = find($db, $table, array('conditions' => array($field => $value)));
+				$dados['status'] = count($res) == 0;
+			}
+		}
+	}
+
+	echo json_encode($dados);
+?>
