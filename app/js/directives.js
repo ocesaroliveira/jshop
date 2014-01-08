9fab26c0abc84aba167b0fce8051849caa2dc3a4
César Oliveira <cesar.o.almeida@gmail.com>
2014-01-08 21:58:22 -0200

    Correção de bugs de sintaxe das diretivas

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,28 +4,30 @@
 
 
 angular.module('myApp.directives', [])
-  	.directive('cortaStr', [function() {
-	    return {
-	    	restrict: 'A',
-	    	scope: {
-	    		text: '=text'
-	    	},
-	    	link: function(scope, elm, attrs) {
-	    		var limiteStr = attrs.limiteStr || 100;
-	    		var txtSubStr = attrs.txtSubStr || '...';
+  	.directive('cortaStr', [
+        function() {
+    	    return {
+    	    	restrict: 'A',
+    	    	scope: {
+    	    		text: '=text'
+    	    	},
+    	    	link: function(scope, elm, attrs) {
+    	    		var limiteStr = attrs.limiteStr || 100;
+    	    		var txtSubStr = attrs.txtSubStr || '...';
 
-	    		var text = scope.text;
-	    		if (text.length > limiteStr) {
-	    			text = text.substr(0, limiteStr);
-	    			text = text.split(' ');
-	    			text = text.splice(0, text.length - 1);
-	    			text = text.join(' ');
-	    			text += txtSubStr;
-	    		}
-    			elm.context.innerHTML = text;
-		    }
-	    }
-  	}
+    	    		var text = scope.text;
+    	    		if (text.length > limiteStr) {
+    	    			text = text.substr(0, limiteStr);
+    	    			text = text.split(' ');
+    	    			text = text.splice(0, text.length - 1);
+    	    			text = text.join(' ');
+    	    			text += txtSubStr;
+    	    		}
+        			elm.context.innerHTML = text;
+    		    }
+    	    }
+        }
+  	])
   	.directive('ngUnique', [
         '$http',
         function($http) {
@@ -43,5 +45,4 @@ angular.module('myApp.directives', [])
                 }
             }
         }
-    ]
-]);
+    ]);
