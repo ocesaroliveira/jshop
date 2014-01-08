4b441d7a5a9f2108821f26dcd3746b44d26b0cbb
César Oliveira <cesar.o.almeida@gmail.com>
2014-01-08 21:41:54 -0200

    Diretivas atualizadas

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,7 +4,29 @@
 
 
 angular.module('myApp.directives', [])
-    .directive('ngUnique', [
+  	.directive('cortaStr', [function() {
+	    return {
+	    	restrict: 'A',
+	    	scope: {
+	    		text: '=text'
+	    	},
+	    	link: function(scope, elm, attrs) {
+	    		var limiteStr = attrs.limiteStr || 100;
+	    		var txtSubStr = attrs.txtSubStr || '...';
+
+	    		var text = scope.text;
+	    		if (text.length > limiteStr) {
+	    			text = text.substr(0, limiteStr);
+	    			text = text.split(' ');
+	    			text = text.splice(0, text.length - 1);
+	    			text = text.join(' ');
+	    			text += txtSubStr;
+	    		}
+    			elm.context.innerHTML = text;
+		    }
+	    }
+  	}
+  	.directive('ngUnique', [
         '$http',
         function($http) {
             return {
@@ -21,4 +43,5 @@ angular.module('myApp.directives', [])
                 }
             }
         }
-    ]);
+    ]
+]);
